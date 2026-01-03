71ebbf3e05f0f273b5caaa9ecd2d41cba31bed53
Ben Ramsey <ben@ramsey.dev>
2026-01-02 18:35:52 -0600

    Modify awk to remove quotes and eliminate call to tr

--- lib/versions.bash
+++ lib/versions.bash
@@ -31,8 +31,7 @@ list_versions() {
 
 	curl "${curl_opts[@]}" -o "$tmp_name" "$static_php_bulk_list" || asdf_fail "Could not download $static_php_bulk_list"
 
-	awk <"$tmp_name" -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/name\042/){print $(i+1)}}}' \
-		| tr -d '"' \
+	awk <"$tmp_name" -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/name\042/){ val=$(i+1); gsub(/"/, "", val); print val }}}' \
 		| grep -o -E "php\-${major}\.${minor}\.${patch}\-${sapi}\-${os}\-${arch}\.tar\.gz" \
 		| sed 's/php-//' \
 		| sed "s/-${sapi}-${os}-${arch}.tar.gz//" \
